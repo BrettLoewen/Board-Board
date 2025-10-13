68373d228d605cf4f0c5d013a974ddc885bc94b3
Brett Loewen <brettzky007@gmail.com>
2025-10-13 15:20:03 -0500

    Simple welcome page

--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -1,7 +1,57 @@
 <script setup lang="ts"></script>
 
 <template>
-  <p>Welcome</p>
+  <div class="container">
+    <div class="welcome">
+      <h1>Welcome to Board Board!</h1>
+      <p>A digital whiteboard for collaborative planning, brainstorming, and organization.</p>
+    </div>
+    <div class="auth-layout">
+      <UButton class="auth-button">Login</UButton>
+      <UButton class="auth-button">Sign Up</UButton>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 200px;
+  min-width: 100%;
+  min-height: 200px;
+}
+
+.welcome {
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 10px;
+}
+.welcome h1 {
+  font-size: 60pt;
+}
+.welcome p {
+  width: 50%;
+  font-size: 15pt;
+}
+
+.auth-layout {
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+  gap: 10px;
+}
+
+.auth-button {
+  justify-content: center;
+  font-size: 12pt;
+}
+.auth-button:active {
+  background-color: var(--color-primary-800);
+}
+</style>
