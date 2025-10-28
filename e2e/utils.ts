858cd944899f49b264ac9d1b17dcbc2c05c7df53
Brett Loewen <brettzky007@gmail.com>
2025-10-28 15:58:11 -0500

    Updated test's db reseed to work on both windows and linux

--- e2e/utils.ts
+++ e2e/utils.ts
@@ -1,4 +1,4 @@
-import { execSync } from "child_process";
+import { execSync, spawnSync } from "child_process";
 import detect from "detect-port";
 import { Page, expect } from "@playwright/test";
 
@@ -17,9 +17,13 @@ async function startSupabase() {
 }
 
 function reseedDb() {
-  execSync(
-    "SET PGPASSWORD=postgres&&psql -U postgres -h 127.0.0.1 -p 54322 -f supabase/clear_db_data.sql",
-    { stdio: "ignore" },
+  spawnSync(
+    "psql",
+    ["-U", "postgres", "-h", "127.0.0.1", "-p", "54322", "-f", "supabase/clear_db_data.sql"],
+    {
+      stdio: "ignore",
+      env: { ...process.env, PGPASSWORD: "postgres" },
+    },
   );
 }
 
